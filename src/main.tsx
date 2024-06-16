75d6ea943f8b44ea3ad479509035f6f10c465477
TheSilkFlower <smirnova100991@gmail.com>
2024-06-16 23:26:14 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { App } from './views/App'
+
+import './views/styles/common.scss'
+import './views/styles/reset.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/views/components/InputTask/index.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import styles from './index.module.scss';
+
+interface InputTaskProps {
+    id: string;
+    title: string;
+    onDone: (id: string) => void;
+    onEdited: (id: string, title: string) => void;
+    onRemoved: (id: string) => void;
+}
+
+export const InputTask: React.FC<InputTaskProps> = ({
+    id,
+    title,
+    onDone,
+    onEdited,
+    onRemoved
+}) => {
+
+    const [checked, setChecked] = useState(false)
+    const [isEditMode, setIsEditMode] = useState(false)
+    const [value, setValue] = useState(title)
+
+    return (
+        <div className={ styles.inputTask }>
+            <label className={ styles.inputTaskLabel }>
+                <input
+                type="checkbox"
+                checked={checked}
+                disabled={isEditMode}
+                className={ styles.inputTaskChecked }
+                onChange={(e) => {
+                    setChecked(e.target.checked)
+                    if(e.target.checked) {
+                        onDone(id)
+                        console.log('sdg');
+                        
+                    }
+                }}
+                />
+                {
+                    isEditMode ? 
+                    <input 
+                    value={value}
+                    onChange={(e) => {
+                        setValue(e.target.value)
+                    }}
+                    className={styles.inputTaskTitleEdit}
+                    /> : 
+                    <h3 className={ styles.inputTaskTitle }>{ title }</h3>
+                }
+                
+            </label>
+            <button
+                aria-label="Edit"
+                className={ styles.inputTaskEdit }
+                onClick={() => {
+                    onEdited(id, title)
+                     setIsEditMode(true)
+                }}
+                />
+            <button
+                aria-label="Edit"
+                className={ styles.inputTaskRemove }
+                onClick={() => {
+                    if(confirm('Are you sure?')) {
+                        onRemoved(id)
+                        console.log('fh');
+                    }
+                }}
+            />
+        </div>
+    )
+}
