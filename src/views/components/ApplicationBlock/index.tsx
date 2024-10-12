d96f36905aef7268d2c414c996860c24de2029e9
TheSilkFlower <smirnova100991@gmail.com>
2024-10-12 11:45:20 +0300

    Создала компонент appBlock

---
+++ src/views/components/ApplicationBlock/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./index.module.scss";
+import { Link } from "@mui/material";
+
+export default function AppBlock() {
+    return (
+        <div className={styles.mainBlock}>
+            <div className={styles.titleBlock}>Создано при помощи:</div>
+            <Link href="https://mui.com/material-ui" target="_blank" underline="none" className={styles.blockLink} sx={{color: "#292d34"}}>@materialUi</Link>
+            <Link href="https://vincentgarreau.com/particles.js/" target="_blank" underline="none" className={styles.blockLink} sx={{color: "#292d34"}}>@tsParticles</Link>
+            <Link href="https://www.typescriptlang.org/" target="_blank" underline="none" className={styles.blockLink} sx={{color: "#292d34"}}>*Typescript</Link>
+            <Link href="https://react.dev/" target="_blank" underline="none" className={styles.blockLink} sx={{color: "#292d34"}}>*React</Link>
+            <Link href="https://vite.dev/" target="_blank" underline="none" className={styles.blockLink} sx={{color: "#292d34"}}>*Vite</Link>
+
+        </div>
+        
+    )
+}
