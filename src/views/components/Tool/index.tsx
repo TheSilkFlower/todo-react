b1907952fc162eb871cea98014c29e5fe24977d8
TheSilkFlower <smirnova100991@gmail.com>
2024-10-15 22:21:08 +0300

    Создала и типизировала компонент Tool

---
+++ src/views/components/Tool/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './index.module.scss';
+import { Link, LinkOwnProps } from "@mui/material";
+
+interface ILink {
+    href: string,
+    target?: string,
+    underline?: LinkOwnProps,
+    className?: string | undefined,
+    sx?: LinkOwnProps,
+    tool: string
+}
+
+export const Tool: React.FC<ILink> = ({...props}) => {
+    return (
+        <Link href={props.href} target="_blank" underline="none" className={styles.blockLink} sx={{color: "#292d34"}}>{props.tool}</Link>
+    )
+}
