51ee117aaa35ef239d11a362a2dc1322f87fada9
TheSilkFlower <smirnova100991@gmail.com>
2024-10-15 22:20:43 +0300

    Импортировала модуль для стилей + изменила время

--- src/views/components/DateTodo/index.tsx
+++ src/views/components/DateTodo/index.tsx
@@ -1,3 +1,5 @@
+import styles from './index.module.scss'
+
 function getTime() {
     let day: number = new Date().getDate()
     let month: number = new Date().getMonth() + 1
@@ -5,12 +7,12 @@ function getTime() {
     let hour: number = new Date().getHours()
     let minutes: number = new Date().getMinutes()
 
-    return `${day}/${month}/${year} ${hour < 10 ? '0' + hour : hour}:${minutes < 10 ? '0' + minutes : minutes}`
+    return `${day}/${month}/${year.toString().slice(2)} ${hour < 10 ? '0' + hour : hour}:${minutes < 10 ? '0' + minutes : minutes}`
 }
 
 export function DateTodo() {
     return (
-        <div>
+        <div className={ styles.date }>
             {getTime()}
         </div>
     )
