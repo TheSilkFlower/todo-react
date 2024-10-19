59fc4848d252eef0750e47e97d35ffe7aa208fd5
TheSilkFlower <smirnova100991@gmail.com>
2024-10-19 12:54:37 +0300

    Обернула компонент TextField в styled

--- src/views/components/InputPlus/index.tsx
+++ src/views/components/InputPlus/index.tsx
@@ -1,12 +1,32 @@
 import React, { useCallback, useState } from "react";
 import styles from './index.module.scss';
-import { Button, TextField } from "@mui/material";
+import { styled, Button, TextField } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import { LightTooltip } from "../LightTooltip";
 
 interface InputPlusProps {
     onAdd: (title: string) => void;
 }
+
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: '#dbefe9',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#708090',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#708090',
+      },
+      '&:hover fieldset': {
+        borderColor: '#dbefe9',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#dbefe9',
+      },
+    },
+  });
 
 export const InputPlus: React.FC<InputPlusProps> = ({
     onAdd,
@@ -16,9 +36,10 @@ export const InputPlus: React.FC<InputPlusProps> = ({
         onAdd(inputValue);
         setInputValue('');
     }, [inputValue])
+      
     return (
         <div className={ styles.inputPlus }>
-            <TextField 
+            <CssTextField 
             id="outlined-basic" 
             label="Введите текст" 
             variant="outlined" 
@@ -31,7 +52,7 @@ export const InputPlus: React.FC<InputPlusProps> = ({
                     addTask()
                 }
             }} >
-            </TextField>
+            </CssTextField>
             <LightTooltip title="Добавить" placement="right">
                 <Button variant={'contained'} className={ styles.inputPlusButton } sx={{background: "transparent", boxShadow: "0 0 2px black"}} size={'medium'} onClick={addTask}>
                 <Add/>
