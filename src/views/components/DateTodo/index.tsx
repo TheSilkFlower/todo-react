5a2ec9d84c0809cc74066160377c8a1b2b3223ca
TheSilkFlower <smirnova100991@gmail.com>
2024-10-20 23:31:12 +0300

    Удалила определение функции getTime

--- src/views/components/DateTodo/index.tsx
+++ src/views/components/DateTodo/index.tsx
@@ -1,17 +1,7 @@
 import { useEffect, useState } from 'react'
 import styles from './index.module.scss'
 import { InputTask } from '../InputTask'
-
-function getTime() {
-    let day: number = new Date().getDate()
-    let month: number = new Date().getMonth() + 1
-    let year: number = new Date().getFullYear()
-    let hour: number = new Date().getHours()
-    let minutes: number = new Date().getMinutes()
-
-    return `${day}/${month}/${year.toString().slice(2)} ${hour < 10 ? '0' + hour : hour}:${minutes < 10 ? '0' + minutes : minutes}`
-}
-
+import { getTime } from '../../../data/stores/useToDoStore'
 
 export function DateTodo() {
     const [time, setTime] = useState('')
