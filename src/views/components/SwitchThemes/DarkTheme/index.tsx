37b96a05115590346a7d3a2c98d8a6198cc77fb6
TheSilkFlower <smirnova100991@gmail.com>
2024-11-03 23:22:09 +0300

    Удалено

--- src/views/components/SwitchThemes/DarkTheme/index.tsx
+++
@@ -1,7 +0,0 @@
-import { DarkMode } from "@mui/icons-material";
-
-export function DarkTheme() {
-    return (
-        <DarkMode />
-    )
-}
--- src/views/components/SwitchThemes/LightTheme/index.tsx
+++
@@ -1,7 +0,0 @@
-import { LightMode } from "@mui/icons-material";
-
-export function LightTheme() {
-    return (
-        <LightMode />
-    )
-}
