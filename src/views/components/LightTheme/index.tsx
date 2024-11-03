ba6b853e636df75b3ce3b9488e03ca1dab15355c
TheSilkFlower <smirnova100991@gmail.com>
2024-11-03 23:37:29 +0300

    Вернула файл с компонентом LightTheme

---
+++ src/views/components/LightTheme/index.tsx
@@ -0,0 +1,8 @@
+import { LightMode } from "@mui/icons-material";
+import styles from './index.module.scss';
+
+export function LightTheme() {
+    return (
+        <LightMode className={ styles.buttonLight }/>
+    )
+}
