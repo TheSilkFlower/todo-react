c4e40f1cb2d58c763c1eec010ac554021fd7a3fd
TheSilkFlower <smirnova100991@gmail.com>
2025-04-09 17:35:27 +0300

    Добавила отступы

--- src/views/components/ApplicationBlock/index.tsx
+++ src/views/components/ApplicationBlock/index.tsx
@@ -3,8 +3,8 @@ import { Tool } from "../Tool";
 
 export default function AppBlock() {
     return (
-        <div className={styles.mainBlock}>
-            <div className={styles.titleBlock}>Создано при помощи:</div>
+        <div className={ styles.mainBlock }>
+            <div className={ styles.titleBlock }>Создано при помощи:</div>
             <Tool href="https://mui.com/material-ui" tool="@materialUi"/>
             <Tool href="https://particles.js.org/" tool="@tsParticles"/>
             <Tool href="https://www.typescriptlang.org/" tool="Typescript"/>
