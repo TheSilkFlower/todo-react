5f3214569c407d2a403891d4b232d69e94844c0a
TheSilkFlower <smirnova100991@gmail.com>
2025-04-10 11:17:47 +0300

    Заменила импорт хранилища

--- src/views/components/SwitchTheme/index.tsx
+++ src/views/components/SwitchTheme/index.tsx
@@ -1,12 +1,12 @@
 import { LightMode } from "@mui/icons-material";
 import { DarkMode } from "@mui/icons-material";
-import { useToDoStore } from "../../../data/stores/useToDoStore";
+import { useThemeStore } from "../../../data/stores/useToDoStore";
 import styles from './index.module.scss';
 
 export function SwitchTheme() {
-    const [ theme, changeTheme ] = useToDoStore(state => [ state.theme, state.changeTheme ]);
+    const [ theme, changeTheme ] = useThemeStore(state => [ state.theme, state.changeTheme ]);
     return (
-        <button className={ styles.mainBtn } onClick={ changeTheme }>
+        <button className={ styles.wrapBtn } onClick={ changeTheme }>
             { theme ? <LightMode className={ styles.buttonLight }/> : <DarkMode className={ styles.buttonDark }/> }
         </button>
     )
